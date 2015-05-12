ed226d8f5b10dd1ccdbdaee881b04043c20fc8e4
nishant <nishant.singh87@gmail.com>
2015-05-12 15:21:58 +0530

    #1 -login-controller : adding error callback on login error

--- src/js/controllers/login-controller.js
+++ src/js/controllers/login-controller.js
@@ -3,15 +3,15 @@
   var $loginButton  = function(){return $("#login-by-id");},
       $idInput      = function(){return $("#login-user-id");};
 
-  var LoginController= function($page, userService, onLogin){
-    var onLoginSuccess = function(user){
-      onLogin(user);
-    };
+  var LoginController= function($page, userService, onLogin, onLoginError){
+    var onLoginSuccess = function (user) {
+          onLogin(user);
+        };
 
     $page.show();
     support.clickAction(function(){
       var userId = support.read($idInput());
-      userService.userById(userId).then(onLoginSuccess);
+      userService.userById(userId).then(onLoginSuccess, onLoginError);
     }).on($loginButton());
   };
 
--- src/test/controllers/login-controller-spec.js
+++ src/test/controllers/login-controller-spec.js
@@ -17,6 +17,16 @@
             then: function (success, failure) {if(id == expectedId) success(user);}
           };
         };
+      },
+
+      errorName = "Login Error"
+      expectedError = "Invalid user id",
+      whenGetUserByIdThrowNotFound = function (expectedId) {
+        userService.userById = function (id) {
+          return {
+            then: function (success, failure) {if(id == expectedId) failure(expectedError);}
+          };
+        };
       };
 
   QUnit.module('LoginController', {
@@ -51,6 +61,29 @@
 
     whenGetUserByIdReturn(expectedUser.id, expectedUser);
     support.input($idInput(), expectedUser.id);
+    support.click($loginButton());
+
+    assert.expect(1);
+  });
+
+  QUnit.test("Should show message if invalid user id", function (assert) {
+    var
+        onLogin = function () {
+          assert.ok(false, "Should not callback for login error");
+        },
+        onError = function (error) {
+          assert.equal(error, expectedError, "Should callback onError with error object");
+        };
+
+    controller = new hightimes.LoginController(
+        $page(),
+        userService,
+        onLogin,
+        onError
+    );
+
+    whenGetUserByIdThrowNotFound("1");
+    support.input($idInput(), "1");
     support.click($loginButton());
 
     assert.expect(1);
